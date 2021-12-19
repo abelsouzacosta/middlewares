0b78cd7f64e9d7b3895cf2a1272c86fb435c59b4
Abel Souza <abelsouzacosta@gmail.com>
2021-12-19 20:19:14 -0300

    feat(checksCreateTodosUserAvailability): middleware
    Verifica se um usuário é pro ou se já possui mais de 10 todos
    cadastrados

--- src/index.js
+++ src/index.js
@@ -25,7 +25,14 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.todos.length >= 10 || !user.pro)
+    return response.status(403).json({
+      error: "User aren't pro, or have more than 10 todos"
+    });
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
