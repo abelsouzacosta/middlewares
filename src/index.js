66250812b55d127af4166525318889c9f6a1d2c5
Abel Souza <abelsouzacosta@gmail.com>
2021-12-19 21:46:10 -0300

    feat(checksTodoExists): middleware
    Verifica se um determinado todo pertence a um usuário

--- src/index.js
+++ src/index.js
@@ -36,7 +36,34 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user)
+    return response.status(404).json({
+      error: "User not found",
+    });
+
+  const idIsUuid = validate(id);
+
+  if (!idIsUuid)
+    return response.status(400).json({
+      error: "Invalid id"
+    });
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo)
+    return response.status(400).json({
+      error: "There's no todo with the id in the given user"
+    });
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
